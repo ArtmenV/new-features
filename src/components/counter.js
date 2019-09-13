56f6703f34fe4d5221a7938a4d91fd4b8bcbb462
Artmen_V <viliutich.artem@gmail.com>
2019-09-13 23:01:19 +0700

    fun with animation

--- src/components/counter.js
+++ src/components/counter.js
@@ -5,7 +5,7 @@ export const Counter = () => {
   const [state, dispatch] = useReducer(counterReducer, initState);
   return (
     <>
-      Count: {state.count}
+      <h1>Count: {state.count}</h1>
       <hr />
       <button onClick={() => dispatch({ type: "increment" })}>+</button>
       <button onClick={() => dispatch({ type: "decrement" })}>-</button>
